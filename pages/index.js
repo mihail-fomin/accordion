ed3a706f65033a2743659b59ad3a51d51bd85544
misha133kv <mihail_fomin_95@mail.ru>
2023-02-05 18:06:29 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import * as React from "react"
+import { twMerge } from "tailwind-merge"
+
+
+function Item({ active, children, onClick }) {
+	const commonCn = `flex inline-center cursor-pointer justify-center w-full p-2 text-xl text-left border-2 border-gray-400 rounded`
+	const activeCn = active ? `
+	text-white bg-blue-600
+	` : ``
+
+	return (
+		<div className={twMerge(commonCn + activeCn)} onClick={onClick} >
+			{children}
+		</div>
+	)
+}
+
+function Accordion({ items }) {
+	const [activeIndex, setactiveIndex] = React.useState(0)
+
+	return (
+		<div>
+			{
+				items.map((item, i) => {
+					return <Item active={activeIndex == i} onClick={() => setactiveIndex(i)}>
+						{item}
+					</Item>
+				})
+			}
+		</div>
+	)
+}
+
+export default function Body() {
+	return <div className="container mx-auto">
+
+		<Accordion items={["First", "Second", "Third"]} />
+		<br />
+		<Accordion items={["A", "B", "C", "D"]} />
+
+	</div>
+}
